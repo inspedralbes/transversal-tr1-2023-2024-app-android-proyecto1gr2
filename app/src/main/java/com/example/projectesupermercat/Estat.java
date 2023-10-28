ba82c09b667d51ff5d3a6b0c52f63a0891b8d69e
MarioMagia <a22marramgar@inspedralbes.cat>
2023-10-28 17:24:34 +0200

    Estat class

---
+++ app/src/main/java/com/example/projectesupermercat/Estat.java
@@ -0,0 +1,25 @@
+package com.example.projectesupermercat;
+
+public enum Estat {
+    PENDENT_DE_CONFIRMACIO(0),
+    EN_PREPARACIO(1),
+    LLEST(2),
+    RECOLLIT(3),
+    INVALID(4);
+
+    private int estat_actual;
+    private Estat(int estat_actual){
+        this.estat_actual = estat_actual;
+    }
+
+    public int getEstat_actual() {
+        return estat_actual;
+    }
+
+    public Estat recibirEstat(int num){
+        if(Estat.values().length-1 >= num){
+            return Estat.INVALID;
+        }
+        return Estat.values()[num];
+    }
+}
