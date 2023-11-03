a893edd1f34ae9e738937a66a1601d96f4a9518e
MrM250 <a22miqdervil@inspedralbes.cat>
2023-11-03 09:43:18 +0100

    funcionen els butons

--- app/src/main/java/com/example/projectesupermercat/PaymentActivity.java
+++ app/src/main/java/com/example/projectesupermercat/PaymentActivity.java
@@ -1,14 +1,54 @@
 package com.example.projectesupermercat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    CardView comandesView, iniciView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        initializeViews();
+        setupListeners();
+
+    }
+
+    private void initializeViews() {
+        comandesView = findViewById(R.id.comandes);
+        iniciView = findViewById(R.id.inici);
+    }
+
+    private void setupListeners() {
+        comandesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleComandesOnClick();
+            }
+        });
+
+        iniciView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleIniciOnClick();
+            }
+        });
+    }
+
+    private void handleComandesOnClick() {
+        Intent intent = new Intent(getApplicationContext(), LlistaComandesActivity.class);
+        startActivity(intent);
+    }
+
+    private void handleIniciOnClick() {
+        Intent intent = new Intent(getApplicationContext(), BasicActivity.class);
+        startActivity(intent);
     }
 }
