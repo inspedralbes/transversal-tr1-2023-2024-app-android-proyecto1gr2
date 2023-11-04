f877c82943d1630ed103a7c53e1f5ae5cf60fe5d
MarioMagia <a22marramgar@inspedralbes.cat>
2023-11-04 16:43:10 +0100

    corregidas sombras de los botones

--- app/src/main/java/com/example/projectesupermercat/BasicActivity.java
+++ app/src/main/java/com/example/projectesupermercat/BasicActivity.java
@@ -244,7 +244,14 @@ public class BasicActivity extends AppCompatActivity implements TotalPriceListen
         precioTotalTextView.setText(decfor.format(totalPrice) + "€");
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
 
+    }
 
     /*@Override
     public boolean onSupportNavigateUp() {
